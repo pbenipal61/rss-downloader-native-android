840bb22d26c133c7d1f2e1c8216addd8ff6a265f
Prabhjot Benipal <t8sipr00@students.oamk.fi>
2019-04-28 23:11:57 +0300

    Improved menu with options to switch between top 10 and top 20. List view now uses ViewHolder pattern

--- app/src/main/java/com/postmaninteractive/fromrssdownloader/MainActivity.java
+++ app/src/main/java/com/postmaninteractive/fromrssdownloader/MainActivity.java
@@ -21,6 +21,10 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
     private ListView listApps;
+    private String feedUrl = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=%d/xml";
+    private int feedLimit = 10;
+    private String feedCachedUrl = "INVALIDATED";
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         listApps = (ListView) findViewById(R.id.xmlListView);
-        downloadUrl("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=25/xml");
+        downloadUrl(String.format(feedUrl,feedLimit));
 
     }
 
@@ -36,32 +40,51 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.feeds_menu,menu);
+
+        if(feedLimit == 10){
+            menu.findItem(R.id.mnu10).setChecked(true);
+        }else{
+            menu.findItem(R.id.mnu25).setChecked(true);
+        }
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        String feedUrl;
 
         switch(id){
             case R.id.mnuFree:
-                feedUrl="http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=25/xml";
+                feedUrl="http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=%d/xml";
                 break;
 
             case R.id.mnuPaid:
-                feedUrl = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/toppaidapplications/limit=25/xml";
+                feedUrl = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/toppaidapplications/limit=%d/xml";
                 break;
 
             case R.id.mnuSongs:
-                feedUrl = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topsongs/limit=25/xml";
+                feedUrl = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topsongs/limit=%d/xml";
+                break;
+                
+            case R.id.mnu10:
+            case R.id.mnu25:
+                if(!item.isChecked()){
+                    item.setChecked(true);
+                    feedLimit = 35 - feedLimit;
+                    Log.d(TAG, "onOptionsItemSelected: " + item.getTitle() + "setting feed limit to " + feedLimit);
+                }else{
+                    Log.d(TAG, "onOptionsItemSelected: " + item.getTitle() + "feed limit unchanged");
+                }
+                break;
+
+            case R.id.mnuRefresh:
                 break;
 
             default:
                 return super.onOptionsItemSelected(item);
         }
 
-        downloadUrl(feedUrl);
+        downloadUrl(String.format(feedUrl,feedLimit));
         return true;
 
     }
